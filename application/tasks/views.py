f285a5d33fc02996b4db25698d2ead6933c0954b
Sofia Riekkola <sofia.riekkola@gmail.com>
2019-09-12 21:42:53 +0300

    Add a function for lists to views.py

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -2,6 +2,10 @@ from application import app, db
 from flask import render_template, request
 from application.tasks.models import Task
 
+@app.route("/tasks", methods=["GET"])
+def tasks_index():
+    return render_template("tasks/list.html", tasks = Task.query.all())
+
 @app.route("/tasks/new/")
 def tasks_form():
     return render_template("tasks/new.html")
