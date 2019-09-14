b412920ba2734bdd06f5bc4276692aebd38f9c2a
Sofia Riekkola <sofia.riekkola@gmail.com>
2019-09-14 19:36:56 +0300

    Change the list of tasks to a list of products

--- application/tasks/models.py
+++ application/tasks/models.py
@@ -1,14 +1,14 @@
 from application import db
 
-class Task(db.Model):
+class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
     onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
-    done = db.Column(db.Boolean, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, name):
+    def __init__(self, name, price):
         self.name = name
-        self.done = False
+        self.price = price
--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,10 +1,10 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
-from application.tasks.models import Task
+from application.tasks.models import Product
 
 @app.route("/tasks", methods=["GET"])
 def tasks_index():
-    return render_template("tasks/list.html", tasks = Task.query.all())
+    return render_template("tasks/list.html", tasks = Product.query.all())
 
 @app.route("/tasks/new/")
 def tasks_form():
@@ -13,7 +13,7 @@ def tasks_form():
 @app.route("/tasks/<task_id>/", methods=["POST"])
 def tasks_set_done(task_id):
 
-    t = Task.query.get(task_id)
+    t = Product.query.get(task_id)
     t.done = True
     db.session().commit()
   
@@ -21,7 +21,7 @@ def tasks_set_done(task_id):
 
 @app.route("/tasks/", methods=["POST"])
 def tasks_create():
-    t = Task(request.form.get("name"))
+    t = Product(request.form.get("name"), 2000)
 
     db.session().add(t)
     db.session().commit()
