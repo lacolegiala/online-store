b6c31bd6744f993611e4b4e6e3467063e6244b9c
Sofia Riekkola <sofia.riekkola@gmail.com>
2019-09-15 14:14:21 +0300

    Change every task reference to product

---
+++ application/products/views.py
@@ -0,0 +1,29 @@
+from application import app, db
+from flask import redirect, render_template, request, url_for
+from application.products.models import Product
+
+@app.route("/products", methods=["GET"])
+def products_index():
+    return render_template("products/list.html", products = Product.query.all())
+
+@app.route("/products/new/")
+def products_form():
+    return render_template("products/new.html")
+  
+@app.route("/products/<product_id>/", methods=["POST"])
+def products_set_done(product_id):
+
+    product = Product.query.get(product_id)
+    product.price = 1000
+    db.session().commit()
+  
+    return redirect(url_for("products_index"))
+
+@app.route("/products/", methods=["POST"])
+def products_create():
+    t = Product(request.form.get("name"), 2000)
+
+    db.session().add(t)
+    db.session().commit()
+  
+    return redirect(url_for("products_index"))
