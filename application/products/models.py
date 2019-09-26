ed18393eb4d3fff84bca9c7cd0dea04554eb2035
Sofia Riekkola <sofia.riekkola@gmail.com>
2019-09-26 21:51:13 +0300

    Add a base class to the new file application/models.py

--- application/products/models.py
+++ application/products/models.py
@@ -1,13 +1,12 @@
 from application import db
+from application.models import Base
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
+class Product(Base):
+
 
     name = db.Column(db.String(144), nullable=False)
     price = db.Column(db.Integer, nullable=False)
+
 
     def __init__(self, name, price):
         self.name = name
--- application/products/views.py
+++ application/products/views.py
@@ -1,6 +1,6 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
-from flask_login import login_required 
+from flask_login import login_required, current_user
 from application.products.models import Product
 from application.products.forms import NewProductForm
 
