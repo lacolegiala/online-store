43b47cb0b38ac7464646bc2ceef8644849bbf6cf
Sofia Riekkola <sofia.riekkola@gmail.com>
2019-10-12 12:04:10 +0300

    Add validators to new product form

--- application/products/forms.py
+++ application/products/forms.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms import IntegerField
-from wtforms.validators import DataRequired
-from wtforms.validators import NumberRange
+from wtforms.validators import DataRequired, NumberRange, length
 
 class NewProductForm(FlaskForm):
-    name = StringField("Product name", validators=[DataRequired()])
+    name = StringField("Product name", validators=[DataRequired(), length(min = 2, max = 100)])
     price = IntegerField("Price", validators=[NumberRange(min = 5, max = 100000)])
  
     class Meta:
