43e4bbb004da97f8388b1bcc4022151ddc103fe3
Sofia Riekkola <sofia.riekkola@gmail.com>
2019-10-15 20:30:47 +0300

    Sort the front page product list by bestselling

--- application/products/models.py
+++ application/products/models.py
@@ -1,6 +1,8 @@
 from application import db
 from application.models import Base
 
+from sqlalchemy.sql import text
+
 class Product(Base):
     name = db.Column(db.String(144), nullable=False)
     price = db.Column(db.Integer, nullable=False)
@@ -9,6 +11,16 @@ class Product(Base):
     def __init__(self, name, price):
         self.name = name
         self.price = price
+
+    @classmethod
+    def order_by_bestselling(cls, products):
+        stmt = text("SELECT * FROM product"
+                    " JOIN store_order_has_product ON product.id = store_order_has_product.product_id "
+                    " GROUP BY store_order_has_product.product_id "
+                    " ORDER BY COUNT(*) DESC")
+        res = db.engine.execute(stmt)
+
+        return res
 
 store_order_has_product = db.Table('store_order_has_product',
     db.Column('store_order_id', db.Integer, db.ForeignKey('store_order.id'), primary_key=True),
@@ -19,3 +31,4 @@ class StoreOrder(Base):
     user_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
     products = db.relationship('Product', secondary=store_order_has_product, lazy='subquery',
         backref=db.backref('orders', lazy=True))
+
--- application/views.py
+++ application/views.py
@@ -1,10 +1,13 @@
 from flask import render_template
 from application import app
-from application.products.models import Product
+from application.products.models import Product, StoreOrder
 from application.auth.models import User, Role
 from flask_login import current_user
 from flask import session
 
 @app.route("/")
 def index():
-    return render_template("index.html", products = Product.query.all())
+
+  productList = Product.order_by_bestselling(Product.query.all())
+
+  return render_template("index.html", products = productList)
